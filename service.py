e4471b8ada4fb6071a767aa5883ee93fb7f254df
ZeroCool <spamnvg@gmail.com>
2019-12-18 08:13:31 -0800

    remote test

--- service.py
+++ service.py
@@ -2,9 +2,10 @@ import win32serviceutil
 import win32service
 import win32event
 import servicemanager
-from multiprocessing import Process
+# from multiprocessing import Process
+from concurrent.futures import ProcessPoolExecutor
 
-# from app import app
+from app import app
 
 class Service(win32serviceutil.ServiceFramework):
     _svc_name_ = "ecmapiwin"
@@ -19,14 +20,16 @@ class Service(win32serviceutil.ServiceFramework):
         self.process.terminate()
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
-    def SvcDoRun(self):
-        self.process = Process(target=self.main)
-        self.process.start()
-        self.process.run()
-
     def main(self):
-        pass
-        # app.run(host="0.0.0.0", debug=False, port=8181)
+        app.run(host="0.0.0.0", debug=False, port=8181)
+
+    def SvcDoRun(self):
+        with ProcessPoolExecutor() as executor:
+            executor.submit(self.main)
+        # self.process = Process(target=self.main)
+        # self.process.start()
+        # self.process.run()
 
 if __name__ == '__main__':
+    # app.run(host="0.0.0.0", debug=False, port=5050)
     win32serviceutil.HandleCommandLine(Service)
