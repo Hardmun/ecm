448b9402d8f840647b46440d3c6f2afb6aa25e7e
Nikulin <nikulin@hq.ecm.ru>
2019-12-21 16:52:41 +0300

    hq-s-1c-dev develop

--- requests_.py
+++ requests_.py
@@ -1,11 +1,11 @@
-import requests
+from requests.auth import HTTPBasicAuth  # or HTTPDigestAuth, or OAuth1, etc.
+from requests import Session
+from zeep import Client
+from zeep.transports import Transport
 
-URL = "http://localhost:8181/api/reports/contracts"
+session = Session()
+session.auth = HTTPBasicAuth("Интеграция".encode("UTF-8"), "123")
+client = Client('http://hq-s-1c-dev.hq.ecm.local/ps_dev/ws/exchangeCons?wsdl',
+            transport=Transport(session=session))
+t = client.service.getresult(paramsCode = "1")
 
-# r = requests.get(url = URL)
-tere: {} = {"fdfdf" : "kdfj"}
-
-def checktype(muststring: str):
-    pass
-
-checktype({})
